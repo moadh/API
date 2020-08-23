bdb4643e94442af85ebf8ce91909b862dac22ece
moadh <moadh.bensassi@kantar.com>
2020-08-23 11:51:22 +0200

    Ajoutez des fichiers projet.

---
+++ API/Startup.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Management.Tracing;
+
+[assembly: ApiConventionType(typeof(DefaultApiConventions))]
+namespace API
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddHealthChecks();
+			services.AddSwaggerDocument();
+			services.AddDistributedTracing(Configuration);
+			services.AddControllers();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseHealthChecks("/health");
+			app.UseOpenApi();
+			app.UseSwaggerUi3();
+			//app.UseReDoc();
+			app.UseRouting();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
